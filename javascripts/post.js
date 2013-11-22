220fc7c352fe5c6dcaafbe6f7274d6ff61d8cd86
Yan Zhao <zhaoyan1117@gmail.com>
2013-11-21 17:50:33 -0800

    Updated search engine id in post.js

--- javascripts/post.js
+++ javascripts/post.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("a.tag").click(function() {
-    var googleCseUrl = "http://www.google.com/cse?cx=017017719923970963940%3Aggcsf-ihsqm&ie=UTF-8&sa=Search"
+    var googleCseUrl = "http://www.google.com/cse?cx=005533343238920809320%3Aewfdhtvdox4&ie=UTF-8&sa=Search"
     var uriEncodedTag = encodeURI($(this).text());
     var tagUrl = googleCseUrl + "&q=" + uriEncodedTag;
     window.location.href = tagUrl;
