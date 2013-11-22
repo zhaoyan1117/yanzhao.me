b3a9ec8b37776683ee2f79888fe2fa222923d405
Yan Zhao <zhaoyan1117@gmail.com>
2013-11-22 00:05:47 -0800

    change on search UI/UX.

--- javascripts/header.js
+++ javascripts/header.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
   $("#search-icon").click(function() {
-    $("#search form").slideDown(function() {
-      $(this).find("input[type=text]").focus();
-    })
+  	var search_form = $("#search form")
+	if (typeof is_down === 'undefined') {is_down = false}
+	if (!is_down) {
+	    search_form.slideDown(function() {
+			$(this).find("input[type=text]").focus();
+	    })
+	    is_down = true;
+	} else {
+		search_form.slideUp(function() {
+	      $(this).find("input[type=text]").focus();
+	    })
+		is_down = false;
+	};      
     return false;
   });
 });
