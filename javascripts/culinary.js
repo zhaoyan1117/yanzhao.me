63481da01f989f6a2b3af0a4964fd56855dcdfb5
Yan Zhao <zhaoyan1117@gmail.com>
2013-11-30 14:50:56 -0800

    Ajax loading food image.

--- javascripts/culinary.js
+++ javascripts/culinary.js
@@ -1,19 +1,31 @@
 $(document).ready(function() {
 
-  $(window).load(function(){
-    $('.food_loading').fadeOut(500);
-    $('.food_folio').delay(500).fadeIn(1000);
+  current_food = 1;
+  already_loading = false;
+  total_food = parseInt($('.food_amount').data('value'));
+
+  function loading_food () {
+    if (!already_loading && current_food <= total_food) {
+      $('.food_loading').fadeIn(250);
+      already_loading = true;
+      $.ajax({
+        url:"/culinary/"+current_food+"/",
+        success:function(result) {
+          $('.food_loading').fadeOut(500);
+          $(result).hide().appendTo(".food_folio").delay(500).fadeIn(1000);
+          current_food++;
+          already_loading = false;
+        }
+      });
+    }
+  }
+
+  $(window).load(loading_food);
+  $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      loading_food();
+    }
   });
 
-  $(".food_container").hover(
-    function(){
-      $(this).stop().animate({
-        boxShadow: '0 0 30px rgba(38, 142, 212, 1.0)'
-      })
-    },
-    function() {
-      $(this).stop().animate({
-        boxShadow: '0px 0px 10px rgba(150,150,150,1.0)'
-      });
-  });
 });
+
