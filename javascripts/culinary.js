e30754097017a52a82bf8ff632244f11a3c46675
Yan Zhao <zhaoyan1117@gmail.com>
2013-11-30 20:24:37 -0800

    Change to load.

--- javascripts/culinary.js
+++ javascripts/culinary.js
@@ -4,23 +4,48 @@ $(document).ready(function() {
   already_loading = false;
   total_food = parseInt($('.food_amount').data('value'));
 
-  function loading_food () {
-    if (!already_loading && current_food <= total_food) {
-      $('.food_loading').fadeIn(500);
-      already_loading = true;
-      $.ajax({
-        url:"/culinary/"+current_food+"/",
-        success:function(result) {
-          $('.food_loading').fadeOut(500);
-          $(result).hide().appendTo(".food_folio").delay(500).fadeIn(1000);
-          current_food++;
-          already_loading = false;
-        }
-      });
+  function check_all_loaded () {
+    if (current_food > total_food) {
+      var food_end = '<div class="food_end">----------- more will be served -----------</div>'
+      $(food_end).hide().appendTo(".food_folio").fadeIn(1000);
+      $(window).unbind('scroll');
     }
   }
 
-  $(window).load(loading_food);
+  function loading_food () {
+    if (!already_loading && current_food <= total_food) {
+      already_loading = true;
+      var food_img = $("<img class='food_img' />").attr('src', '/culinary/images/'+current_food+'.png')
+        .load(function() {
+          $(food_img).hide().appendTo(".food_folio").fadeIn(1000);
+          current_food++;
+          already_loading = false;
+      });
+    }
+
+    check_all_loaded();
+  }
+
+  function init_loading_food () {
+    if (!already_loading && current_food <= total_food) {
+      already_loading = true;
+      var food_img_1 = $("<img class='food_img' />").attr('src', '/culinary/images/'+current_food+'.png')
+        .load(function() {
+          current_food++;
+          var food_img_2 = $("<img class='food_img' />").attr('src', '/culinary/images/'+current_food+'.png')
+            .load(function() {
+              current_food++;
+              $(food_img_1).hide().appendTo(".food_folio").fadeIn(1000);
+              $(food_img_2).hide().appendTo(".food_folio").fadeIn(1000);
+              already_loading = false;
+            });
+      });
+    }
+
+    check_all_loaded();
+  }
+
+  $(window).load(init_loading_food);
   $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() >= $(document).height()*0.99) {
       loading_food();
